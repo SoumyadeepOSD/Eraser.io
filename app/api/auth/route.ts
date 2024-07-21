20280a3544d383cadae9db14849e34e8031e499d
SoumyadeepOSD <opencontainer255@gmail.com>
2024-07-22 02:15:21 +0530

    fixed code in api route

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -1,7 +1,17 @@
-import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
+import { handleAuth, AuthEndpoints } from "@kinde-oss/kinde-auth-nextjs/server";
 import { NextRequest } from "next/server";
 
-export async function GET(request: NextRequest, {params}: any) {
-  const endpoint = params.kindeAuth;
-  return await handleAuth(request, endpoint);
+export async function GET(request: NextRequest, context: { params: { kindeAuth: AuthEndpoints } }) {
+  const { kindeAuth } = context.params;
+  
+  if (!kindeAuth) {
+    return new Response("Missing 'kindeAuth' parameter", { status: 400 });
+  }
+
+  try {
+    return await handleAuth(request, kindeAuth);
+  } catch (error) {
+    console.error("Error in GET handler:", error);
+    return new Response("Internal Server Error", { status: 500 });
+  }
 }
